8c5a1a8683a95fd112dad065f4c2f85d472aa053
Adam K Dean <adamkdean@googlemail.com>
2016-04-23 23:41:39 +0100

    Final bug commit. Added in all other views + hooked up data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('nomoreunhandledrejections')();
+
 const express = require('express');
 const cons = require('consolidate');
 const path = require('path');
@@ -11,10 +13,15 @@ const httpPort = process.env.HTTP_PORT || 3000;
 const api = axios.create({ baseURL: process.env.API_URL || 'http://localhost:8000/' });
 const app = express();
 
+const baseViewModel = {
+  posterUrlPrefix: 'https://image.tmdb.org/t/p/w300'
+};
+
 //
 // Setup
 //
 app.engine('dust', cons.dust);
+cons.dust.helpers = require('dustjs-helpers');
 app.set('view engine', 'dust');
 app.set('views', path.join(workdir, 'views'));
 
@@ -40,7 +47,7 @@ const simpleCache = {
       value: data
     };
   }
-}
+};
 
 //
 // API wrappers
@@ -61,6 +68,8 @@ const apiWrapper = async (endpoint) => {
 
 const getPopularFilms = async (when = 'last_week') => await apiWrapper(`/films?when=${when}`);
 const getPopularReviews = async (when = 'last_week') => await apiWrapper(`/reviews?when=${when}`);
+const getFilmBySlug = async (slug) => await apiWrapper(`/film/${slug}`);
+const getReviewsByFilmSlug = async (slug) => await apiWrapper(`/reviews/${slug}`);
 
 //
 // Routes
@@ -68,43 +77,46 @@ const getPopularReviews = async (when = 'last_week') => await apiWrapper(`/revie
 app.get('/', async (req, res) => {
   const films = await getPopularFilms();
   const reviews = await getPopularReviews();
-
-  console.log(
-    films.length,
-    reviews.length
-  );
-
-  res.render('index', {});
+  const viewModel = Object.assign({}, baseViewModel, { films, reviews });
+  res.render('index', viewModel);
 });
 
-app.get('/films', (req, res) => {
-  res.render('films', {
-    title: 'Films - SEEN'
+app.get('/films', async (req, res) => {
+  const lastWeek = await getPopularFilms('last_week');
+  const lastMonth = await getPopularFilms('last_month');
+  const lastYear = await getPopularFilms('last_year');
+  const allTime = await getPopularFilms('all_time');
+  const viewModel = Object.assign({}, baseViewModel, {
+    title: 'Films - SEEN',
+    lastWeek,
+    lastMonth,
+    lastYear,
+    allTime
   });
+  res.render('films', viewModel);
 });
 
-app.get('/film/:slug', (req, res) => {
-  res.render('film', {
-    title: 'Film (Single) - SEEN'
-  });
+app.get('/film/:slug', async (req, res) => {
+  const slug = req.params.slug;
+  const film = await getFilmBySlug(slug);
+  const reviews = await getReviewsByFilmSlug(slug);
+  const viewModel = Object.assign({}, baseViewModel, { title: `${film.title} - SEEN`, film, reviews });
+  res.render('film', viewModel);
 });
 
-app.get('/reviews', (req, res) => {
-  res.render('reviews', {
-    title: 'Reviews - SEEN'
+app.get('/reviews', async (req, res) => {
+  const thisWeek = await getPopularReviews('last_week');
+  const thisMonth = await getPopularReviews('last_month');
+  const thisYear = await getPopularReviews('last_year');
+  const allTime = await getPopularReviews('all_time');
+  const viewModel = Object.assign({}, baseViewModel, {
+    title: 'Reviews - SEEN',
+    thisWeek,
+    thisMonth,
+    thisYear,
+    allTime
   });
-});
-
-app.get('/review/:slug', (req, res) => {
-  res.render('review', {
-    title: 'Review (Single) - SEEN'
-  });
-});
-
-app.get('/search', (req, res) => {
-  res.render('search', {
-    title: 'Search - SEEN'
-  });
+  res.render('reviews', viewModel);
 });
 
 //
