0639a112afca40f5f21161ea016169acf596f3ef
Adam K Dean <adamkdean@googlemail.com>
2016-04-23 11:48:15 +0100

    Added views + mapped them to routes

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ const path = require('path');
 const morgan = require('morgan');
 const scssMiddleware = require('./middleware/scss');
 const staticMiddleware = require('./middleware/static');
-const app = express();
 
+const workdir = process.cwd();
 const httpPort = process.env.HTTP_PORT || 3000;
+const app = express();
 
 //
 // Setup
 //
 app.engine('dust', cons.dust);
 app.set('view engine', 'dust');
-app.set('views', path.join(__dirname, '..', 'views'));
+app.set('views', path.join(workdir, 'views'));
 
 app.use(morgan('combined'));
 app.use(scssMiddleware);
@@ -22,9 +23,39 @@ app.use('/public', staticMiddleware);
 //
 // Routes
 //
-app.get('/', (req, res, next) => {
-  res.render('layout', {
-    title: 'index'
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'SEEN'
+  });
+});
+
+app.get('/films', (req, res) => {
+  res.render('films', {
+    title: 'Films - SEEN'
+  });
+});
+
+app.get('/film/:slug', (req, res) => {
+  res.render('film', {
+    title: 'Film (Single) - SEEN'
+  });
+});
+
+app.get('/reviews', (req, res) => {
+  res.render('reviews', {
+    title: 'Reviews - SEEN'
+  });
+});
+
+app.get('/review/:slug', (req, res) => {
+  res.render('review', {
+    title: 'Review (Single) - SEEN'
+  });
+});
+
+app.get('/search', (req, res) => {
+  res.render('search', {
+    title: 'Search - SEEN'
   });
 });
 
