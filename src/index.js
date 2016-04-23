49ec8e4eca4c895fdc5f47b9bcc09a4eeefac4cd
Adam K Dean <adamkdean@googlemail.com>
2016-04-23 02:22:55 +0100

    Initial build of website. WIP. Time for bed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-(async () => {
-  console.log('working?');
+const express = require('express');
+const cons = require('consolidate');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+
+const httpPort = process.env.HTTP_PORT || 3000;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '..', 'public'), { redirect: false }));
+
+app.engine('dust', cons.dust);
+app.set('view engine', 'dust');
+app.set('views', path.join(__dirname, '..', 'views'));
+
+app.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'hello world'
+  });
 });
+
+app.listen(httpPort, () => console.log(`Listening on port ${httpPort}`));
