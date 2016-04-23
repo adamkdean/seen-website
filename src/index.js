cdeaf1dfd8f36e53d16fe9111300aaffe0a8a6c3
Adam K Dean <adamkdean@googlemail.com>
2016-04-23 10:46:48 +0100

    Added in scss middleware + split out static serving

--- src/index.js
+++ src/index.js
@@ -2,21 +2,33 @@ const express = require('express');
 const cons = require('consolidate');
 const path = require('path');
 const morgan = require('morgan');
+const scssMiddleware = require('./middleware/scss');
+const staticMiddleware = require('./middleware/static');
 const app = express();
 
 const httpPort = process.env.HTTP_PORT || 3000;
 
-app.use(morgan('combined'));
-app.use('/public', express.static(path.join(__dirname, '..', 'public'), { redirect: false }));
-
+//
+// Setup
+//
 app.engine('dust', cons.dust);
 app.set('view engine', 'dust');
 app.set('views', path.join(__dirname, '..', 'views'));
 
+app.use(morgan('combined'));
+app.use(scssMiddleware);
+app.use('/public', staticMiddleware);
+
+//
+// Routes
+//
 app.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'hello world'
+  res.render('layout', {
+    title: 'index'
   });
 });
 
+//
+// 3... 2... 1...
+//
 app.listen(httpPort, () => console.log(`Listening on port ${httpPort}`));
