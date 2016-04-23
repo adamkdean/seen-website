7c42ae5ee8fa6b03b334e341a43317e068591d74
Adam K Dean <adamkdean@googlemail.com>
2016-04-23 12:48:04 +0100

    Added simple cache + api wrappers. Now I have to go to a wedding. :(

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # website
 
-TMDB images are stored @ https://image.tmdb.org/t/p/w300{/ID.jpg}
+## Notes
+
+* TMDB images are stored @ https://image.tmdb.org/t/p/w300{/ID.jpg}
+
+## TODO:
+
+(for both of the above, stick it on app?)
+1. Split cache out into middleware layer? [app.simpleCache]
+2. Split api wrappers out into middleware layer? [app.api.{fn}]
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cons = require('consolidate');
 const path = require('path');
 const morgan = require('morgan');
+const axios = require('axios');
 const scssMiddleware = require('./middleware/scss');
 const staticMiddleware = require('./middleware/static');
 
 const workdir = process.cwd();
 const httpPort = process.env.HTTP_PORT || 3000;
+const api = axios.create({ baseURL: process.env.API_URL || 'http://localhost:8000/' });
 const app = express();
 
 //
@@ -21,12 +23,58 @@ app.use(scssMiddleware);
 app.use('/public', staticMiddleware);
 
 //
+// Simple cache
+//
+const simpleCache = {
+  cache: [],
+  expiration: 60, // seconds
+  timestamp: () => Math.floor(Date.now() / 1000),
+  get: function get(key) {
+    return (this.cache[key] && this.cache[key].validUntil >= this.timestamp())
+      ? this.cache[key].value
+      : delete this.cache[key] && null;
+  },
+  set: function set(key, data) {
+    return this.cache[key] = {
+      validUntil: this.timestamp() + this.expiration,
+      value: data
+    };
+  }
+}
+
+//
+// API wrappers
+//
+const apiWrapper = async (endpoint) => {
+  let data = simpleCache.get(endpoint);
+
+  if (data === null) {
+    const result = await api.get(endpoint);
+    if (result.status === 200) {
+      simpleCache.set(endpoint, result.data);
+      data = result.data;
+    }
+  }
+
+  return data;
+};
+
+const getPopularFilms = async (when = 'last_week') => await apiWrapper(`/films?when=${when}`);
+const getPopularReviews = async (when = 'last_week') => await apiWrapper(`/reviews?when=${when}`);
+
+//
 // Routes
 //
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'SEEN'
-  });
+app.get('/', async (req, res) => {
+  const films = await getPopularFilms();
+  const reviews = await getPopularReviews();
+
+  console.log(
+    films.length,
+    reviews.length
+  );
+
+  res.render('index', {});
 });
 
 app.get('/films', (req, res) => {
