2e11014e99b21654796a9d4bf2f92b5b9dd1ddc2
Adam K Dean <adamkdean@googlemail.com>
2016-04-24 01:28:03 +0100

    Updated deployment script to not masquerade as api

--- build/docker.sh
+++ build/docker.sh
@@ -5,13 +5,13 @@ ETH1_IP=$(ifconfig eth1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print 
 
 API_URL="http://$ETH1_IP:3000/"
 
-docker kill api 2>/dev/null || true
-docker rm -f api 2>/dev/null || true
+docker kill website 2>/dev/null || true
+docker rm -f website 2>/dev/null || true
 docker run \
   --detach \
   --restart=on-failure:5 \
   --publish $ETH0_IP:80:80 \
-  --name api \
+  --name website \
   --env HTTP_PORT=80 \
   --env API_URL=$API_URL \
   quay.io/seenproject/website
